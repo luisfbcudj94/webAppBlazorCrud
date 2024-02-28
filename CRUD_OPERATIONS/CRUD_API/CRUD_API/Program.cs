b7d712e70334806d71b5d9e603ee53cb925b3eab
LuisPerez <alejandroabril98@gmail.com>
2024-02-28 17:36:27 -0500

    feat: Implement navigation between different table pages.

--- CRUD_OPERATIONS/CRUD_API/CRUD_API/Program.cs
+++ CRUD_OPERATIONS/CRUD_API/CRUD_API/Program.cs
@@ -36,11 +36,14 @@ builder.Services.AddScoped<IUsersRepository, UsersRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("MyCors");
