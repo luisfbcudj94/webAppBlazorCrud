44ea401bbe0486f3560d3588b7f892d283a6c749
LuisPerez <alejandroabril98@gmail.com>
2024-02-29 14:14:38 -0500

    feat: add custom validations

---
+++ CRUD_OPERATIONS/FrontEnd/FrontEnd/Helpers/DataManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FrontEnd.Helpers
+{
+    public static class DataManager
+    {
+        public static string BuildErrorMessageHtml(List<string> missingFields)
+        {
+            var errorMessageHtml = "<p>You must fill out the following fields:</p><div>";
+
+            foreach (var field in missingFields)
+            {
+                errorMessageHtml += $"<p><b style='color: red;'>{field}</b></p>";
+            }
+
+            errorMessageHtml += "</div>";
+
+            return errorMessageHtml;
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            string emailPattern = @"^[^\s@]+@[^\s@]+\.[^\s@]+$";
+            var isEmail = Regex.IsMatch(email, emailPattern);
+            return isEmail;
+        }
+    }
+}
